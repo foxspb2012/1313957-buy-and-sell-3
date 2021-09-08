e099359ca45d6a840e70935ae1e39e75eb18d578
Евгений <foxspb2012@gmail.com>
2021-09-08 15:43:48 +0300

    Правки по заданию: "Учебный проект: хрустящий CRUD"

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -4,24 +4,22 @@ const {Router} = require(`express`);
 const category = require(`../api/category`);
 const offer = require(`../api/offer`);
 const search = require(`../api/search`);
-
 const getMockData = require(`../lib/get-mock-data`);
 
-const {
-        CategoryService,
-        SearchService,
-        OfferService,
-        CommentService,
-      } = require(`../data-service`);
+const receiveMockData = async () => {
+  const {
+          CategoryService,
+          SearchService,
+          OfferService,
+          CommentService,
+        } = require(`../data-service`);
 
-const app = new Router();
+  const app = new Router();
+  const mockData = await getMockData();
 
-const mockData = async () => {
-  return await getMockData();
+  category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
+  offer(app, new OfferService(mockData), new CommentService());
 };
 
-category(app, new CategoryService(mockData));
-search(app, new SearchService(mockData));
-offer(app, new OfferService(mockData), new CommentService());
-
-module.exports = app;
+module.exports = receiveMockData;
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -10,32 +10,31 @@ const DEFAULT_PORT = 3000;
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.info(chalk.green(`Request on route ${req.url}`));
-  res.on(`finish`, () => {
-    console.info(`Response status code ${res.statusCode}`);
-  });
-  next();
-});
-
-app.use(API_PREFIX, routes);
-
-app.use((req, res) => {
-  res.status(HttpCode.NOT_FOUND)
-    .send(`Not found`);
-  console.error(`Route not found: ${req.url}`);
-});
-
-app.use((err, _req, _res, _next) => {
-  console.error(`An error occured on processing request: ${err.message}`);
-});
-
-
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    app.use((req, res, next) => {
+      console.info(chalk.green(`Request on route ${req.url}`));
+      res.on(`finish`, () => {
+        console.info(`Response status code ${res.statusCode}`);
+      });
+      next();
+    });
+
+    app.use(API_PREFIX, routes);
+
+    app.use((req, res) => {
+      res.status(HttpCode.NOT_FOUND)
+        .send(`Not found`);
+      console.error(`Route not found: ${req.url}`);
+    });
+
+    app.use((err, _req, _res, _next) => {
+      console.error(`An error occured on processing request: ${err.message}`);
+    });
 
     try {
       app.listen(port, (err) => {
